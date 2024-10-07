482c5b9b85becfd394781905ee5b9a4ced307b2e
nlormsby <nlormsby@gmail.com>
2024-10-07 04:15:54 -0700

    Added stage scraping

--- gamatotoScrape.py
+++ gamatotoScrape.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import requests
 import pandas as pd
+from requests.structures import CaseInsensitiveDict
 import time
 from pandas import json_normalize
 
+# Get a list to scrape
 cat_url = 'https://mygamatoto.com/comparecats/'
 listCheck = input("Do you have a My Gamatoto Cat List? (yes/no)")
 
@@ -43,15 +45,29 @@ for cat in cats:
     Range = cat.find_element("xpath", './/td[8]').text
     KB = cat.find_element("xpath", './/td[9]').text
     Speed = cat.find_element("xpath", './/td[10]').text
-    DPS = cat.find_element("xpath", 'td[11]/div').text
-    Target = cat.find_element("xpath", 'td[12]').text
-    TBA = cat.find_element("xpath", 'td[13]').text
-    Animation = cat.find_element("xpath", 'td[14]').text
-    Respawn = cat.find_element("xpath", 'td[15]').text
-    Cost = cat.find_element("xpath", 'td[16]').text
-    Type = cat.find_element("xpath", 'td[17]').text
-    Abilities = cat.find_element("xpath", 'td[18]').text
+    DPS = cat.find_element("xpath", './/td[11]/div').text
+    Target = cat.find_element("xpath", './/td[12]').text
+    TBA = cat.find_element("xpath", './/td[13]').text
+    Animation = cat.find_element("xpath", './/td[14]').text
+    Respawn = cat.find_element("xpath", './/td[15]').text
+    Cost = cat.find_element("xpath", './/td[16]').text
 
+    type_list = cat.find_element("xpath", './/td[17]').text
+    types = list(type_list.split(", "))
+    for type in types:
+        if type == ""
+
+
+    # underline = cat.find_elements("xpath", './/td[18]/div*')
+    # bold_check = False
+    # for word in underline:
+    #     print(word.text)
+    #     if word.text.startswith("Against"):
+    #         Against = list(word.text[8:].split(", "))
+    #         bold_check = True
+
+
+    Abilities = cat.find_element("xpath", './/td[18]').text
 
     cat_item = {
         'Name': Name,
@@ -84,13 +100,61 @@ driver.get(url)
 input("Open a stage and press Enter to continue...")
 
 enemy_tables = driver.find_elements("css selector", '.ant-table-body > .ant-table-fixed > tbody')
-enemies = enemy_tables[1].find_elements("css selector", './/tr')
+enemies = enemy_tables[1].find_elements("xpath", './/tr')
 
 enemy_list = []
+enemy_api = 'https://onestoppress.com/api/singleenemy'
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/json"
+type_list = {"red", "floating", "black", "metal", "white", "angel", "alien", "zombie", "aku", "relic"}
 
 for enemy in enemies:
-    Name = cat.find_element("xpath", './/td[2]/a').text
+    Name = enemy.find_element("xpath", './/td[1]/a/img').get_attribute('alt')
+    Amplifier = enemy.find_element("xpath", './/td[2]/div').text
+    Number = enemy.find_element("xpath", './/td[3]').text
+    Base = enemy.find_element("xpath", './/td[4]').text
+    Time = enemy.find_element("xpath", './/td[5]').text
+    Interval = enemy.find_element("xpath", './/td[6]').text
+    Boss = enemy.find_element("xpath", './/td[7]').text
 
+    api_name = '{"compare":"' + Name + '"}'
+    print(api_name)
+    r = requests.post(url=enemy_api, headers=headers, data=api_name)
+    data = r.json()
+    print(data)
+    enemy_data = data['sampledata']
+
+
+    Speed = enemy_data["speed"]
+    Range = enemy_data["range"]
+    Target = enemy_data["att_type"]
+    Ability = enemy_data["special_att_desc"]
+    Type = []
+
+    for type in type_list:
+        if enemy_data[type] == True:
+            Type.append(type)
+
+
+    enemy_item = {
+        'Name': Name,
+        'Amplifier': Amplifier,
+        'Number': Number,
+        'Base': Base,
+        'Time': Time,
+        'Interval': Interval,
+        'Boss': Boss,
+        'Speed': Speed,
+        'Range': Range,
+        'Target': Target,
+        'Ability': Ability,
+        'Type': Type
+    }
+    enemy_list.append(enemy_item)
+
+typed_cats = df[cat_frame['Type'] == 'Red']
+enemy_frame = pd.DataFrame(enemy_list)
+print(enemy_frame)
 
 #cat row class:  ant-table-row editable-row ant-table-row-level-0
 #name path:  //*[@id="root"]/div/div[2]/section/section/main/div[2]/div/div/div/div/div[1]/div[1]/div[2]/table/tbody/tr[2]/td[2]/a
